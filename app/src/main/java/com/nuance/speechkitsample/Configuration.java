6b82715a08f1d10e4a072bffd6e4868361400112
Amir Bawab <amirbawab@gmail.com>
2018-01-27 15:15:25 -0500

    Init nuance files

---
+++ app/src/main/java/com/nuance/speechkitsample/Configuration.java
@@ -0,0 +1,34 @@
+package com.nuance.speechkitsample;
+
+import android.net.Uri;
+
+import com.nuance.speechkit.PcmFormat;
+
+/**
+ * All Nuance Developers configuration parameters can be set here.
+ *
+ * Copyright (c) 2015 Nuance Communications. All rights reserved.
+ */
+public class Configuration {
+
+    //All fields are required.
+    //Your credentials can be found in your Nuance Developers portal, under "Manage My Apps".
+    public static final String APP_KEY = "14d785f757f29788be28f64e127253512870712ae1220f377b43746de87667fc4decf4a4de8b2c9cb422ebc6943e17bcae89e78fabf6c6263a3a9422b0fb1efd";
+    public static final String APP_ID = "NMDPTRIAL_amir_elbawab_mail_mcgill_ca20180127145707";
+    public static final String SERVER_HOST = "nmsps.dev.nuance.com";
+    public static final String SERVER_PORT = "443";
+
+    public static final String LANGUAGE = "eng-USA";
+
+    public static final Uri SERVER_URI = Uri.parse("nmsps://" + APP_ID + "@" + SERVER_HOST + ":" + SERVER_PORT);
+
+    //Only needed if using NLU
+    public static final String CONTEXT_TAG = "M10216_A3228";
+
+    public static final PcmFormat PCM_FORMAT = new PcmFormat(PcmFormat.SampleFormat.SignedLinear16, 16000, 1);
+    public static final String LANGUAGE_CODE = (Configuration.LANGUAGE.contains("!") ? "eng-USA" : Configuration.LANGUAGE);
+
+}
+
+
+
