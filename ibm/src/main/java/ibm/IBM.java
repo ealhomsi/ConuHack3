440d184e4b5457747cf7b1a3d610c9e14a0ed3ee
Amir Bawab <amirbawab@gmail.com>
2018-01-27 17:46:29 -0500

    Created a sample IBM program

--- ibm/src/main/java/ibm/IBM.java
+++ ibm/src/main/java/ibm/IBM.java
@@ -1,7 +1,39 @@
 package ibm;
 
+import com.ibm.watson.developer_cloud.http.ServiceCall;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class IBM {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        VisualRecognition service = new VisualRecognition(
+                VisualRecognition.VERSION_DATE_2016_05_20
+        );
+        service.setApiKey("7818f2e5f1bf15336fbdda1bcd220d332e80deab");
+
+        // Read input from user
+        String fileName = System.getProperty("image");
+
+        InputStream imagesStream = null;
+        try {
+            imagesStream = new FileInputStream(fileName);
+            ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+                    .imagesFile(imagesStream)
+                    .imagesFilename(fileName)
+                    .parameters(
+                            "{ \"classifier_ids\": [\"default\"], \"threshold\": 0.6}")
+                    .build();
+            ClassifiedImages result = service.classify(classifyOptions).execute();
+            System.out.println(result);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
